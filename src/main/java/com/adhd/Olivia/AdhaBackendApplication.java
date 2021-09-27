8937aaedcba0299be4b6a1bece20fa713f381eb7
device999 <sattari4@hotmail.com>
2021-09-27 16:10:39 +0200

    Initial step

---
+++ src/main/java/com/adhd/Olivia/AdhaBackendApplication.java
@@ -0,0 +1,13 @@
+package com.adhd.Olivia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdhaBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdhaBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/adhd/Olivia/AdhaBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.adhd.Olivia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdhaBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
