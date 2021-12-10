ebdab474aa1d1d8be13ab50d1cea61a60e0f7b92
AnjaTRPES <roeder.anja.sci@gmail.com>
2021-12-10 18:08:57 +0100

    google signin and login work

--- src/main/java/com/adhd/Olivia/controllers/RegisterController.java
+++ src/main/java/com/adhd/Olivia/controllers/RegisterController.java
@@ -81,25 +81,8 @@ public class RegisterController {
 		}			
 	}
 	
-	
-	@PostMapping("/signup-google")
-	public ResponseEntity<String> newUserByGoogle(@RequestBody User user) throws MessagingException, JsonProcessingException{
-			System.out.println(user.getFullName());
-			List<User> emailBasedUsers = userRepo.findByEmail(user.getEmail());
-			List<User> loginBasedUsers = userRepo.findByLogin(user.getLogin());
-			if(emailBasedUsers.size()>0) {
-				return new ResponseEntity<String>("Email exists",HttpStatus.FORBIDDEN);
-			}
-			userRepo.save(user);
-			Map<String,Object> response = new HashMap<>();
-			response.put("userId",user.getId());
-			response.put("name",user.getFullName());
-			String json = new ObjectMapper().writeValueAsString(response);
-			//mailService.sendEmail(MailTypes.signUp(user.getEmail()));
-			return new ResponseEntity<String>(json,HttpStatus.CREATED);
-		}
 
-	@PostMapping("signup-google2/{token}")
+	@PostMapping("signup-google/{token}")
 	public ResponseEntity<String> newUserGoogle2(@PathVariable String token) throws IOException {
 		// from https://developers.google.com/identity/sign-in/web/backend-auth -> don't understand how that works...
 		URL url = new URL("https://www.googleapis.com/oauth2/v3/userinfo");
@@ -194,25 +177,58 @@ public class RegisterController {
         }		
 	}
 	
-	@PostMapping("/login-google/{userEmail}")
-	public ResponseEntity<String> loginByGoogle(@PathVariable String userEmail)throws JsonProcessingException{
-		List<User> logedInUser = userRepo.findByEmail(userEmail);
-		if(logedInUser.size()==1) {
-			Map<String,Object> response = new HashMap<>();
-			response.put("userId",logedInUser.get(0).getId());
-			response.put("name",logedInUser.get(0).getFullName());
-			Optional<Questionarrie> firstTime = questionRepo.findByUser(logedInUser.get(0));
-			if(firstTime.isPresent()) {
-				response.put("firstTime",false);
-			}else {
-				response.put("firstTime",true);
+	@PostMapping("login-google/{token}")
+	public ResponseEntity<String> loginByGoogle2(@PathVariable String token) throws IOException {
+		// from https://developers.google.com/identity/sign-in/web/backend-auth -> don't understand how that works...
+		URL url = new URL("https://www.googleapis.com/oauth2/v3/userinfo");
+		HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+		conn.setRequestMethod("GET");
+		conn.setRequestProperty("Content-Type", "application/json");
+		conn.setRequestProperty("Authorization", "Bearer "+token);
+		conn.connect();
+		//get the response
+		int responseCode = conn.getResponseCode();
+		if (responseCode != 200) {
+			return new ResponseEntity<String>("Error connecting to Google", HttpStatus.BAD_REQUEST);
+		} else {
+			String json = "";
+			BufferedReader br = new BufferedReader(new InputStreamReader((conn.getInputStream()), "UTF-8"));
+			StringBuilder sb = new StringBuilder();
+			String output;
+			while ((output = br.readLine()) != null) {
+				sb.append(output);
 			}
-			String responseJson = new ObjectMapper().writeValueAsString(response);
-			return new ResponseEntity<String>(responseJson,HttpStatus.OK);
-		}else {
-			return new ResponseEntity<String>("Not Found",HttpStatus.NOT_FOUND);
+			json = sb.toString();
+			//Use the objectMapper just as for the regular login -> won't work as it's not
+			ObjectMapper mapper = new ObjectMapper();
+			try{
+				Map<String, String> map = mapper.readValue(json, Map.class);
+				String email = map.get("email");
+				// check if email exists already
+				List<User> emailBasedUsers = userRepo.findByEmail(email);
+				if(emailBasedUsers.size()==1) {
+					Map<String,Object> response = new HashMap<>();
+					response.put("userId",emailBasedUsers.get(0).getId());
+					response.put("name",emailBasedUsers.get(0).getFullName());
+					Optional<Questionarrie> firstTime = questionRepo.findByUser(emailBasedUsers.get(0));
+					if(firstTime.isPresent()) {
+						response.put("firstTime",false);
+					}else {
+						response.put("firstTime",true);
+					}
+					String responseJson = new ObjectMapper().writeValueAsString(response);
+					return new ResponseEntity<String>(responseJson,HttpStatus.OK);
+				} else {
+					return new ResponseEntity<String>("Not Found",HttpStatus.NOT_FOUND);
+				}
+
+			} catch (IOException e) {
+				return new ResponseEntity<String>("Server Error for map",HttpStatus.INTERNAL_SERVER_ERROR);
+			}
 		}
+
 	}
+
 	
 	@GetMapping("/facebook")
 	public ResponseEntity<String> loginByFacebook(@RequestBody String email,@RequestBody String password){
