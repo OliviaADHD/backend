41080a4917d77ac25cde0719ed264e37b035a982
device999 <sattari4@hotmail.com>
2022-01-05 02:10:32 +0100

    Initial file transfer

--- src/main/java/com/adhd/Olivia/controllers/ProfileController.java
+++ src/main/java/com/adhd/Olivia/controllers/ProfileController.java
@@ -1,16 +1,22 @@
 package com.adhd.Olivia.controllers;
 
-import java.util.Map;
+
+import java.io.IOException;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.util.StringUtils;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
+import com.adhd.Olivia.custom.FileUploadUtil;
 import com.adhd.Olivia.enums.Language;
 import com.adhd.Olivia.models.db.Profile;
 import com.adhd.Olivia.models.db.User;
@@ -19,8 +25,6 @@ import com.adhd.Olivia.repo.UserRepository;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
-
-
 
 @RestController
 @RequestMapping("/profile")
@@ -33,6 +37,27 @@ public class ProfileController {
 	
 	@Autowired
 	public ProfileRepository profileRepo;
+	
+	@PostMapping("/{userId}/photo")
+	public ResponseEntity<String> uploadPhoto(@RequestParam("image") MultipartFile multipartFile,@PathVariable int userId){
+		String fileName = StringUtils.cleanPath(multipartFile.getOriginalFilename());
+		User usr = userRepo.findById(userId);
+		Optional<Profile> optProf = profileRepo.findByUser(usr);
+		if(optProf.isEmpty()) {
+			return new ResponseEntity<String>("Server Error",HttpStatus.INTERNAL_SERVER_ERROR);
+		}
+		Profile prof = optProf.get();
+		prof.setProfilePhoto(fileName);
+		profileRepo.save(prof);
+ 
+        String uploadDir = "user-photos/" + userId; 
+        try {
+			FileUploadUtil.saveFile(uploadDir, fileName, multipartFile);
+		} catch (IOException e) {
+			return new ResponseEntity<String>(e.getMessage(),HttpStatus.OK);	
+		}
+		return new ResponseEntity<String>("Server Error",HttpStatus.INTERNAL_SERVER_ERROR);	
+	}
 	
 	@PostMapping("/preferences")
 	public ResponseEntity<String> preferencesSave(@RequestBody String json){
@@ -80,4 +105,6 @@ public class ProfileController {
 			return new ResponseEntity<String>("Server Error",HttpStatus.INTERNAL_SERVER_ERROR);	
 		}	
 	}
+	
+
 }
